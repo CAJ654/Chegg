0051e4c0fbe4782c15e91fe686915b319f4a0709
Christian Diaz <cajgamer654@gmail.com>
2026-03-11 00:41:04 +0000

    Add sudo to packages

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,24 +1,18 @@
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
   channel = "stable-24.11";
-  # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.nodejs_20
     pkgs.docker
+    pkgs.sudo
   ];
-  # Sets environment variables in the workspace
-  env = {};
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      "usernamehw.errorlens"
     ];
-    # Enable previews and customize configuration
     previews = {
       enable = true;
       previews = {
         web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
+          command = ["npm" "run" "dev" "--" "-p" "$PORT" "--hostname" "0.0.0.0"];
           manager = "web";
         };
       };
