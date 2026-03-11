88ab8d84b6f193fbd915554a24d18b9c3ad2e3be
Christian Diaz <cajgamer654@gmail.com>
2026-03-11 02:13:00 +0000

    Make sure my dockerfile is up to date

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,28 +1,24 @@
 { pkgs, ... }: {
-  channel = "stable-24.11";
+  channel = "stable-24.05";
   packages = [
     pkgs.nodejs_20
     pkgs.docker
-    pkgs.docker-compose
   ];
   idx = {
     extensions = [
-      "bradlc.vscode-tailwindcss"
-      "dsznajder.es7-react-js-snippets"
+      "usernamehw.errorlens"
+      "esbenp.prettier-vscode"
     ];
     workspace = {
       onCreate = {
         npm-install = "npm install";
-      };
-      onStart = {
-        # Add any startup commands here
       };
     };
     previews = {
       enable = true;
       previews = {
         web = {
-          command = [ "npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0" ];
+          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
           manager = "web";
         };
       };
